265b77a02ddeb85191134eddfddf2cba174d4f7d
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2024-08-25 15:14:43 +0000

    Finalizei codigo de datas

--- nextdate.c
+++ nextdate.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int intevaloVAlido(int d, int m, int a)
+int intervaloValido(int d, int m, int a)
 {
-
     if (1 > d || d > 31)
     {
         printf("Dia fora do Intervalo\n");
@@ -24,25 +23,39 @@ int intevaloVAlido(int d, int m, int a)
     return 0;
 }
 
+int diasNoMes(int m, int a)
+{
+    switch (m)
+    {
+    case 4: case 6: case 9: case 11:
+        return 30;
+    case 2:
+        if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0))
+            return 29; // Ano bissexto
+        else
+            return 28;
+    default:
+        return 31;
+    }
+}
+
 void proximaData(int d, int m, int a)
 {
-    int ver = 1;
-    ver = intevaloVAlido(d, m, a);
+    int ver = intervaloValido(d, m, a);
 
     if (ver == 1)
     {
         printf("------\n");
     }
     else
-    { // Mudar aqui para contar 30 e 31
+    {
         d = d + 1;
-        if (d > 31)
+        if (d > diasNoMes(m, a))
         {
             d = 1;
             m = m + 1;
             if (m > 12)
             {
-                d = 1;
                 m = 1;
                 a = a + 1;
             }
@@ -53,7 +66,6 @@ void proximaData(int d, int m, int a)
 
 int main(void)
 {
-
     int d, m, a;
 
     printf("Insira os valores: \n");
